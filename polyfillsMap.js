931e7b6c3188742f6aed788695c5155fc172815d
favibe <ibefavour85@gmail.com>
2025-10-14 19:52:07 +0100

    polyfills for map

--- polyfillsMap.js
+++ polyfillsMap.js
@@ -4,3 +4,16 @@ console.log(squaredNum);
 
 /*To use polyfills  for  older browsers; we have to include Prototype*/
 //Array.prototype.myMap : THE myMap  is  my function represebting the map function' 
+function getSquared(number) {
+    return number ** 2;
+}
+Array.prototype.myMap = function(callback) {
+    let tempArr = [];
+    for (let i = 0; i < this.length; i++) {
+        tempArr.push(callback(this[i], i, this));
+    }
+    return tempArr;
+};
+
+const squ = arr.myMap(getSquared);
+console.log(squ);
