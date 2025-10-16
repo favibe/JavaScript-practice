9403f18cfbfdf6f109890f9a5c466e79bf328020
favibe <ibefavour85@gmail.com>
2025-10-16 11:29:30 +0100

    promises definition

---
+++ asychronous/promise.js
@@ -0,0 +1,10 @@
+/* promise -promise is an object that has the status of an object of an async operation,
+and its coressponding value */
+
+/* in the callback hell the code base increase horinzontally which makes it messay, 
+not readable(nested callbacks)--- Happens when asychronous operations depends on each other.
+but the codebase should  rather increase verticllly (where promisee $ ---async/await) comes in...
+
+---> Promise  is like a placeholder for a value that you'll get in the future.
+it is crfeated using the new promise() constructor */
+
