a4ec968a2d3802be78e994d3e4a32f2a03293bf5
favibe <ibefavour85@gmail.com>
2025-10-16 09:23:30 +0100

    Create callbackhell.js

---
+++ asychronous/callbackhell.js
@@ -0,0 +1,19 @@
+//callback hell -> pyriamid of done -> inversion of control
+
+//bookHotel --> processPayment --> showBookingStatus --> updateBookingHistory
+
+function bookHotel() {
+
+}
+
+function processPayment () {
+
+}
+
+function showBookingStatus () {
+
+}
+
+function updateBookingHistory() {
+
+}
