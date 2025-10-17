e185ecfdffb659d712d7653b6f6ef3ebfc8d01b0
favibe <ibefavour85@gmail.com>
2025-10-17 20:45:07 +0100

    Update class.js

--- classes/class.js
+++ classes/class.js
@@ -1,1 +1,20 @@
 //Class: 
+
+class User {
+    constructor(name, role,isAdmin, isLoggedIn) {
+        (this.name = name),
+        (this.role = role),
+        (this.isAdmin = isAdmin),
+        (this.isLoggedIn = isLoggedIn);
+    }
+    displayInfo() {
+        console.log(`${this.name} is a ${this.role}`)
+    }
+}
+
+const user1 = new User("Joy", "Data Ananlyst", false , true);
+const user2 = new User("Fav", "Full stsck web Developer", true , true)
+console.log(user1);
+console.log(user2);
+
+
